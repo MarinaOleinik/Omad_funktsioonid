b7a10bcba3876844e9cf99ca77f11ee09d8cc8a4
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2021-11-10 12:27:46 +0200

    Add project files.

---
+++ Modul.py
@@ -0,0 +1,34 @@
+def arithmetic(a: float,b:float,c:str):
+    """Lihtne kalkulaator
+    + - liitmine, - - lahutamine, * - korrutamine,/ - jagamine
+    :param float a: Esimene arv
+    :param float b: Teine arv
+    :param str c: Aritmeetiline tehing
+    :rtype var: Märamata tüüb
+    """
+    if c=="+":
+        r=a+b
+    elif c=="-":
+        r=a-b
+    elif c=="*":
+        r=a*b
+    elif c=="/":
+        if b!=0:
+            r=a/b
+        else:
+            r="Div/0"
+    else:
+        r="Tundnatu sym"
+    return r
+
+def is_year_leap(aasta: int):
+    """Liigaasta leidmine
+    Tagastab true kui aasta on liigaasta ja False kui ei ole
+    :param int aasta: Aasta number
+    :rtype bool: Funktsioni vastus loogilises formaadis
+    """
+    if aasta%4==0:
+        vastus=True
+    else:
+        vastus=False
+    return vastus
